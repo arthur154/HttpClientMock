f431433dea1f70bcb99853acf5aa4291208ee668
marthurIntersys <39538915+marthurIntersys@users.noreply.github.com>
2019-08-07 21:50:48 -0700

    Example project (#3)
    * Initial example project commit
    
    * Additional test examples
    
    * Finalize example solution.

---
+++ example/Service/HttpClientDependentService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ExampleHttpClientMock.Service
+{
+    public interface IHttpClientDependentService
+    {
+        IEnumerable<string> GetValues();
+        string GetValue(int id);
+    }
+
+    public class HttpClientDependentService : IHttpClientDependentService
+    {
+        private HttpClient _httpClient;
+
+        public HttpClientDependentService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public IEnumerable<string> GetValues()
+        {
+            string rawValues;
+            string[] returnValue;
+
+            try
+            {
+                rawValues = _httpClient.GetAsync("https://localhost:44386/api/values").Result.Content.ReadAsStringAsync().Result;
+            }
+            catch
+            {
+                throw new Exception("Caught exception while calling api/values.");
+            }
+
+
+            try
+            {
+                returnValue = JsonConvert.DeserializeObject<string[]>(rawValues);
+            }
+            catch
+            {
+                throw new Exception("Caught exception while parsing result from api/values.");
+            }
+
+            return returnValue;
+        }
+
+        public string GetValue(int id)
+        {
+            string rawValue;
+
+            try
+            {
+                rawValue = _httpClient.GetAsync($"https://localhost:44386/api/values/{id}").Result.Content.ReadAsStringAsync().Result;
+            }
+            catch
+            {
+                throw new Exception($"Caught exception while calling api/values/{id}.");
+            }
+
+            return rawValue;
+        }
+    }
+}

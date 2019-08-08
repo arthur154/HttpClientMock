33033a5bc9b3d1670b0d4e66a5ef39abe172c6a5
Mitchell <marthur@intersysconsulting.com>
2019-08-07 20:46:47 -0700

    Initial example project commit

---
+++ example/Controllers/ValuesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ExampleHttpClientMock.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleHttpClientMock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        IHttpClientDependentService _httpClientDependentService;
+
+        public ValuesController (IHttpClientDependentService httpClientDependentService)
+        {
+            _httpClientDependentService = httpClientDependentService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            string returnValue;
+
+            if (id == 1)
+            {
+                returnValue = "Some Value";
+            }
+            else if (id == 2)
+            {
+                returnValue = "Another Value";
+            }
+            else
+            {
+                throw new Exception("No value found.");
+            }
+
+            return returnValue;
+        }
+
+        // GET api/values/httpClientDependency
+        [HttpGet("httpClientDependency")]
+        public ActionResult<IEnumerable<string>> GetWithDependency()
+        {
+            return Ok(_httpClientDependentService.GetValues());
+        }
+
+        // GET api/values/httpClientDependency/5
+        [HttpGet("httpClientDependency/{id}")]
+        public ActionResult<string> GetWithDependency(int id)
+        {
+            return Ok(_httpClientDependentService.GetValue(id));
+        }
+    }
+}
---
+++ example/Service/HttpClientDependentService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ExampleHttpClientMock.Service
+{
+    public interface IHttpClientDependentService
+    {
+        IEnumerable<string> GetValues();
+        string GetValue(int id);
+    }
+
+    public class HttpClientDependentService : IHttpClientDependentService
+    {
+        private HttpClient _httpClient;
+
+        public HttpClientDependentService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public IEnumerable<string> GetValues()
+        {
+            var rawValues = _httpClient.GetAsync("https://localhost:44386/api/values").Result.Content.ReadAsStringAsync().Result;
+            return JsonConvert.DeserializeObject<string[]>(rawValues);
+        }
+
+        public string GetValue(int id)
+        {
+            var rawValue = _httpClient.GetAsync($"https://localhost:44386/api/values/{id}").Result.Content.ReadAsStringAsync().Result;
+            return rawValue;
+        }
+    }
+}
